e73d904d4c741b36ff1f8745410406841534ad7e
ke127 <ke.guitar@gmail.com>
2017-12-17 15:20:29 +0900

    gitよくわからん

---
+++ app/src/main/java/com/example/kegui/mysize/HeightActivity.java
@@ -0,0 +1,13 @@
+package com.example.kegui.mysize;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HeightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_height);
+    }
+}
